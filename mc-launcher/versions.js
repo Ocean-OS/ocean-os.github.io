818eb09e1e3fde5e9a7a1065a38fb0b9c48aee72
ComputerGuy <63362464+Ocean-OS@users.noreply.github.com>
2023-09-14 22:26:38 -0700

    Create versions.js

---
+++ mc-launcher/versions.js
@@ -0,0 +1,51 @@
+window.versionList = [
+  {
+    version: "0.3 Pre-Alpha",
+    url: "https://ocean-os.github.io/mc-assets/0.3",
+    folder: "0.3PA",
+    files: [
+      "index.html",
+      "main.js",
+      "minecraft.mp3",
+      "script.js",
+      "main.js",
+      "icon.ico"
+    ],
+    changelog: "The first released version of Mine.craft! This version has only one biome: Forest. Basically just bare bones. Have fun!"
+  },
+  {
+    version: "0.46a Pre-Alpha",
+    url: "https://ocean-os.github.io/mc-assets/0.46",
+    folder: "0.46aPA",
+    files: [
+      "index.html",
+      "main.js",
+      "icon.ico",
+      "Aria%20Math.mp3",
+      "Biome%20Fest.mp3",
+      "Blind%20Spots.mp3",
+      "Clark.mp3",
+      "Danny.mp3",
+      "Dreiton.mp3",
+      "Dry%20Hands.mp3",
+      "Haggstrom.mp3",
+      "Haunt%20Muskie.mp3",
+      "Ki.mp3",
+      "Living%20Mice.mp3",
+      "Minecraft.mp3",
+      "Moog%20City%202.mp3",
+      "Moog%20City.mp3",
+      "Shuniji.mp3",
+      "Subwoofer%20Lullaby.mp3",
+      "Wet%20Hands.mp3",
+      "Chris.mp3",
+      "Beginning.mp3",
+      "devtools-detect.js",
+      "mods.js",
+      "offline-sounds.js",
+      "preload.js",
+      "script.js"
+    ],
+    changelog: "The second released version of Mine.craft! There were a few versions in between, but they were either unstable or just didn't work well (if you're curious, the names of these versions were: 0.33, 0.4, 0.43). This version added working TNT, a Desert biome and a Cherry biome, block breaking and placing sounds, mod support, a bigger soundtrack, water, and many more blocks! Enjoy!"
+  }
+]
