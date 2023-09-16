115a9e9ce11e4f406edf9d496c4e86076a9bfdd5
ComputerGuy <63362464+Ocean-OS@users.noreply.github.com>
2023-09-16 10:36:42 -0700

    Update script.js

--- mc-assets/0.46b/script.js
+++ mc-assets/0.46b/script.js
@@ -1,5 +1,5 @@
 //Do not try to run this in a webpage. This will only work in the launcher. 
-const version = "0.46a Pre-Alpha";
+const version = "0.46b Pre-Alpha";
 window.version = version;
 getModFiles();
 /**
