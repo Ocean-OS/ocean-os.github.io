9ba324614b6980992108129ce64b52374341c96a
ComputerGuy <63362464+Ocean-OS@users.noreply.github.com>
2023-09-23 08:53:18 -0700

    Update mods.js

--- mc-assets/0.46b/mods.js
+++ mc-assets/0.46b/mods.js
@@ -1,5 +1,9 @@
 localStorage.clear();
 var gameMods = [];
+const fs = require('fs');
+window.fs = fs;
+const path = require('path');
+window.path = path;
 window.gameShaders = [];
 function getModFiles() {
     var justCreated;
@@ -59,4 +63,69 @@ function runMods(){
 }
 function getModIds() {
     return allMods;
+}
+//Music
+var musicFiles = [
+  "./Aria Math.mp3",
+"./Biome Fest.mp3",
+"./Blind Spots.mp3",
+"./Clark.mp3",
+"./Danny.mp3",
+"./Dreiton.mp3",
+"./Dry Hands.mp3",
+"./Haggstrom.mp3",
+"./Haunt Muskie.mp3",
+"./Living Mice.mp3",
+"./Mice On Venus.mp3",
+"./Minecraft.mp3",
+"./Moog City 2.mp3",
+"./Moog City.mp3",
+"./Shuniji.mp3",
+"./Subwoofer Lullaby.mp3",
+"./Wet Hands.mp3",
+"./Ki.mp3",
+"./Chris.mp3",
+"./Beginning.mp3",
+"./otherside.mp3"];
+window.onload = function(){
+var lastMusic;
+var musicPlaying = new Audio();
+var gettingMusic = true;
+var musicUnverifiedFiles = [];
+var musicVerifiedFiles = [];
+var checkingMusic = true;
+for(var musicCheck = 0; musicCheck < musicFiles.length; musicCheck++){
+  if(!fs.existsSync(musicFiles[musicCheck])){
+    musicUnverifiedFiles.push(musicFiles[musicCheck]);
+  }
+}
+for(var musicAdd = 0; musicAdd < musicFiles.length; musicAdd++){
+  if(!musicFiles.includes(musicUnverifiedFiles[musicAdd])){
+    musicVerifiedFiles.push(musicFiles[musicAdd]);
+  }
+}
+musicFiles = musicVerifiedFiles;
+checkingMusic = false;
+musicPlaying.src = musicFiles[Math.round(Math.random()*(musicFiles.length-1))];
+lastMusic = musicPlaying.src;
+musicPlaying.loop = false;
+musicPlaying.play();
+musicPlaying.addEventListener("ended", function(){
+  var gettingMusic = true;
+  while(gettingMusic && lastMusic !== musicPlaying.src){
+    musicPlaying.src = musicFiles[Math.round(Math.random()*(musicFiles.length-1))];
+    if(musicPlaying.src !== lastMusic){
+      lastMusic = musicPlaying.src;
+      musicPlaying.play();
+      gettingMusic = false;
+    }else{
+      gettingMusic = true;
+    }
+  }
+});
+musicPlaying.onpause = function(){
+    if(!musicPause){
+        musicPlaying.play();
+    }
+}
 }
