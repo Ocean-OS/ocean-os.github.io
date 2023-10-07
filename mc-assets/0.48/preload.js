8a4698df0871d0b910cbf76fecd82c108ab85cd6
ComputerGuy <63362464+Ocean-OS@users.noreply.github.com>
2023-10-07 11:17:45 -0700

    Create preload.js

---
+++ mc-assets/0.48/preload.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', () => {
+  window.nodeVersion = process.versions['node'];
+  window.chromeVersion = process.versions['chrome'];
+  window.electronVersion = process.versions['electron'];
+});
