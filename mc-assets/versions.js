effc9cd4dfac03fd6565e47027226acb3fa059da
ComputerGuy <63362464+Ocean-OS@users.noreply.github.com>
2023-10-07 10:48:49 -0700

    Update versions.js

--- mc-assets/versions.js
+++ mc-assets/versions.js
@@ -85,5 +85,48 @@ window.versionList = [
       "script.js"
     ],
     changelog: "A minor update to 0.46a, this version introduces a new chunk rerendering system; to activate it, press R (when F3 is on). This version also adds a few new splash texts, procedurally chosen trees types, distance-based block breaking/placing sound volume, savable settings, and changes the crosshair, among a few other small changes. Enjoy!"
-  }
+  },
+  /*{
+  version: "0.48 Pre-Alpha",
+  url: "https://ocean-os.github.io/mc-assets/0.48",
+  folder: "0.48PA",
+  files: [
+  
+      "index.html",
+      "main.js",
+      "icon.ico",
+      "Aria%20Math.mp3",
+      "Biome%20Fest.mp3",
+      "Blind%20Spots.mp3",
+      "Clark.mp3",
+      "Danny.mp3",
+      "Dreiton.mp3",
+      "Dry%20Hands.mp3",
+      "Haggstrom.mp3",
+      "Haunt%20Muskie.mp3",
+      "Ki.mp3",
+      "Living%20Mice.mp3",
+      "Minecraft.mp3",
+      "Moog%20City%202.mp3",
+      "Moog%20City.mp3",
+      "Shuniji.mp3",
+      "otherside.mp3",
+      "Subwoofer%20Lullaby.mp3",
+      "Wet%20Hands.mp3",
+      "Chris.mp3",
+      "otherside.mp3",
+      "Beginning.mp3",
+      "otherside.mp3",
+      "otherside.mp3",
+      "Taswell.mp3",
+      "Beginning%202.mp3",
+      "Mutation.mp3",
+      "Dead%20Voxel.mp3",
+      "devtools-detect.js",
+      "mods.js",
+      "preload.js",
+      "script.js"
+      ],
+      changelog: "A somewhat big update to 0.46b, 0.48 improves how TNT behaves, changes gravity when underwater, adds new music and splash text, adds view bobbing, and stained glass. Have fun!",
+}*/
 ]
